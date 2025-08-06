b9c009deea13947fa6bf5e68971995b96b4421a3
Santhosh Raaj K R <santhoshraajkr.17@gmail.com>
2025-08-06 17:59:12 +0530

    finalizing for POC

--- src/components/Plagiarism-upload.jsx
+++ src/components/Plagiarism-upload.jsx
@@ -75,7 +75,7 @@ export default function AIPlagiarismChecker() {
                   className="w-10 h-10 object-contain"
                 />
                 <h1 className="text-3xl font-bold text-white">
-                  AI <span className="bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent">Plagiarism</span> Checker
+                Code <span className="bg-gradient-to-r from-cyan-400 to-blue-400 bg-clip-text text-transparent">Plagiarism</span> Checker
                 </h1>
               </div>
               <p className="text-slate-300 text-sm mx-auto max-w-2xl">
